ee080ead0b20ea404a210ac6b0dd973dabee9d94
Martin Raszyk <martin.raszyk@dfinity.org>
2025-05-13 11:48:24 +0200

    fix: overflow when computing cycles consumption

--- canfund-rs/src/manager/record.rs
+++ canfund-rs/src/manager/record.rs
@@ -51,8 +51,8 @@ impl CanisterRecord {
             self.previous_cycles = Some(previous_cycles.clone());
             // Timestamp difference is in nanoseconds, so we need to multiply by 1_000_000_000 to get cycles per second.
             self.consumption_history.add_sample(
-                previous_cycles.amount.saturating_sub(cycles.amount) as u64 * 1_000_000_000
-                    / cycles.timestamp.saturating_sub(previous_cycles.timestamp),
+                (previous_cycles.amount.saturating_sub(cycles.amount) * 1_000_000_000
+                    / cycles.timestamp.saturating_sub(previous_cycles.timestamp) as u128) as u64,
             );
         }
 
